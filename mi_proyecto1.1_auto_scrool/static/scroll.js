af516740f88179161d3d5d3dfd29f1a7c8807678
Deecuador <aportandoaa@gmail.com>
2024-12-02 07:40:46 -0500

    scroll.js

--- mi_proyecto1.1_auto_scrool/static/scroll.js
+++ mi_proyecto1.1_auto_scrool/static/scroll.js
@@ -13,7 +13,7 @@ document.getElementById("scrollButton").addEventListener("click", function() {
         scrolling = false;
         this.textContent = "Iniciar Scroll";
       }
-    }, 50); // Ajusta la velocidad (más bajo = más rápido)
+    }, 80); // Ajusta la velocidad (más bajo = más rápido)
   } else {
     clearInterval(scrollInterval);
     scrolling = false;
